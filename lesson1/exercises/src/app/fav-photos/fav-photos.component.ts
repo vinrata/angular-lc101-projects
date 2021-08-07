1ff2bb75827424cf85ea1be5b47f754720211c58
vinrata <vinnycamma@gmail.com>
2021-08-07 16:46:28 -0500

    finished lesson1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Happy Images Space';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cdn2.bulbagarden.net/upload/b/b0/Celebi_M04.png';
+  image3 = 'https://www.dierbergs.com/-/media/Corporate/Images/Galleries/Images/539-x-279-FAQ.ashx?h=279&la=en&w=539';
 
   constructor() { }
 
